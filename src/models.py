0ddb8a61f121124389a04f2d7a82bb9862d8d375
ralcadev09 <121913575+ralcadev09@users.noreply.github.com>
2025-06-24 04:30:26 +0000

    proyecto finalizado, diagrama QuickDBD adjunto

--- src/models.py
+++ src/models.py
@@ -1,19 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorites: Mapped[list['FavoriteCharacters']] = relationship(back_populates='user')
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25), nullable=False)
+    height: Mapped[int] = mapped_column(Integer, nullable=True)
+    weight: Mapped[int] = mapped_column(Integer)
+    favorite_by: Mapped[list['FavoriteCharacters']] = relationship(back_populates=('character'))
+
+class FavoriteCharacters(db.Model):
+    __tablename__ = 'favorite_characters'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped['User'] = relationship(back_populates='favorites')
+    character_id: Mapped[int] = mapped_column(ForeignKey('characters.id'))
+    character: Mapped['Characters'] = relationship(back_populates='favorite_by')
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25), nullable=False)
+    population: Mapped[int] =mapped_column(Integer, nullable=True)
+    weather: Mapped[str] = mapped_column(String(25)) 
+    favorite_by: Mapped[list['FavoritePlanet']] = relationship(back_populates=('planet'))
+
+
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite_planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped['User'] = relationship(back_populates='favorites')
+    character_id: Mapped[int] = mapped_column(ForeignKey('characters.id'))
+    character: Mapped['Characters'] = relationship(back_populates='favorite_by')
+
+
+
+
+
+
+
+
+    
